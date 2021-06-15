59a0fd76693a4d7b43399582e35292b4b6da8a68
liuqi <469227928@qq.com>
2021-06-15 18:04:45 +0800

    init opengauss_meetings

--- opengauss_meetings/urls.py
+++ opengauss_meetings/urls.py
@@ -32,8 +32,8 @@ schema_view = get_schema_view(openapi.Info(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('meetings.urls')),
+    # path('admin/', admin.site.urls),
+    path('opengauss/', include('meetings.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),name='schema-redoc')
 ]
