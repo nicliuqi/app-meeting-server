c474424d8f50d26f5ba160291cd91d7ad3c49020
liuqi <469227928@qq.com>
2022-04-20 17:40:33 +0800

    # 添加日历

--- meetings/send_email.py
+++ meetings/send_email.py
@@ -1,6 +1,12 @@
+import datetime
+import icalendar
 import logging
+import pytz
 import re
 import smtplib
+import uuid
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -9,7 +15,7 @@ from django.conf import settings
 logger = logging.getLogger('log')
 
 
-def sendmail(topic, date, start, join_url, sig_name, toaddrs, summary=None, record=None, enclosure_paths=None):
+def sendmail(topic, date, start, end, join_url, sig_name, toaddrs, summary=None, record=None, enclosure_paths=None):
     start_time = ' '.join([date, start])
     toaddrs = toaddrs.replace(' ', '').replace('，', ',').replace(';', ',').replace('；', ',')
     toaddrs_list = toaddrs.split(',')
@@ -57,6 +63,45 @@ def sendmail(topic, date, start, join_url, sig_name, toaddrs, summary=None, reco
             file = MIMEApplication(open(file_path, 'rb').read())
             file.add_header('Content-Disposition', 'attachment', filename=file_path)
             msg.attach(file)
+
+    # 添加日历
+    dt_start = (datetime.datetime.strptime(date + ' ' + start, '%Y-%m-%d %H:%M') - datetime.timedelta(hours=8)).replace(tzinfo=pytz.utc)
+    dt_end = (datetime.datetime.strptime(date + ' ' + end, '%Y-%m-%d %H:%M') - datetime.timedelta(hours=8)).replace(tzinfo=pytz.utc)
+
+    cal = icalendar.Calendar()
+    cal.add('prodid', '-//openeuler conference calendar')
+    cal.add('version', '2.0')
+    cal.add('method', 'REQUEST')
+
+    event = icalendar.Event()
+    event.add('attendee', toaddrs_string)
+    event.add('organizer', 'public@openeuler.org')
+    event.add('summary', topic)
+    event.add('dtstart', dt_start)
+    event.add('dtend', dt_end)
+    event.add('dtstamp', dt_start)
+    event['uid'] = uuid.uuid4()
+    event.add('priority', 5)
+    event.add('sequence', 1)
+
+    alarm = icalendar.Alarm()
+    alarm.add('action', 'DISPLAY')
+    alarm.add('description', 'Reminder')
+    alarm.add('TRIGGER;RELATED=START', '-PT1H')
+    event.add_component(alarm)
+
+    cal.add_component(event)
+
+    filename = 'invite.ics'
+    part = MIMEBase('text', 'calendar', method='REQUEST', name=filename)
+    part.set_payload(cal.to_ical())
+    encoders.encode_base64(part)
+    part.add_header('Content-Description', filename)
+    part.add_header('Content-class', 'urn:content-classes:calendarmessage')
+    part.add_header('Filename', filename)
+    part.add_header('Path', filename)
+
+    msg.attach(part)
 
     # 完善邮件信息
     msg['Subject'] = topic
