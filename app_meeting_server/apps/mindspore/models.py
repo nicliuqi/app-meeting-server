a3fa64da3e7b8e1dd2e829f3acbe229c6dd3a2dd
TomNewChao <tom_toworld@163.com>
2023-10-17 10:45:04 +0800

    opt the models

--- app_meeting_server/apps/mindspore/models.py
+++ app_meeting_server/apps/mindspore/models.py
@@ -1,27 +1,9 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from app_meeting_server.utils.models import BaseUser, BaseMeeting
 from django.db import models
 
 
-class User(AbstractBaseUser):
+class User(BaseUser):
     """用户表"""
-    nickname = models.CharField(verbose_name='昵称', max_length=40, null=True, blank=True)
-    gitee_name = models.CharField(verbose_name='gitee名称', max_length=40, null=True, blank=True)
-    avatar = models.CharField(verbose_name='用户头像', max_length=255, null=True, blank=True)
-    openid = models.CharField(verbose_name='openid', max_length=32, unique=True, null=True, blank=True)
-    password = models.CharField('密码', max_length=128, null=True, blank=True)
-    level = models.SmallIntegerField(verbose_name='权限级别', choices=((1, '普通用户'), (2, '授权用户'), (3, '管理员')),
-                                     default=1)
-    activity_level = models.SmallIntegerField(verbose_name='活动权限', choices=((1, '普通用户'), (2, '授权用户'), (3, '管理员')),
-                                              default=1)
-    signature = models.CharField(verbose_name='个性签名', max_length=255, blank=True, null=True)
-    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True, null=True, blank=True)
-    last_login = models.DateTimeField(verbose_name='上次登录时间', auto_now=True, null=True, blank=True)
-    agree_privacy_policy = models.BooleanField(verbose_name='同意隐私政策', default=False)
-    agree_privacy_policy_time = models.DateTimeField(verbose_name='同意隐私政策时间', null=True, blank=True)
-    agree_privacy_policy_version = models.CharField(verbose_name='同意隐私政策版本', max_length=20, null=True, blank=True)
-    revoke_agreement_time = models.DateTimeField(verbose_name='撤销同意隐私声明时间', null=True, blank=True)
-    is_delete = models.SmallIntegerField(verbose_name='是否删除', choices=((0, '否'), (1, '是')), default=0)
-    logoff_time = models.DateTimeField(verbose_name='注销时间', null=True, blank=True)
 
     USERNAME_FIELD = 'openid'
 
@@ -79,32 +61,14 @@ class CityUser(models.Model):
         verbose_name_plural = verbose_name
 
 
-class Meeting(models.Model):
+class Meeting(BaseMeeting):
     """会议表"""
-    topic = models.CharField(verbose_name='会议主题', max_length=128)
-    community = models.CharField(verbose_name='社区', max_length=40, null=True, blank=True)
-    group_name = models.CharField(verbose_name='组名', max_length=40, default='')
     group_type = models.SmallIntegerField(verbose_name='组别', choices=((1, 'SIG'), (2, 'MSG'), (3, 'Pro')))
     city = models.CharField(verbose_name='城市', max_length=10, null=True, blank=True)
-    sponsor = models.CharField(verbose_name='发起人', max_length=20)
-    date = models.CharField(verbose_name='会议日期', max_length=30)
-    start = models.CharField(verbose_name='会议开始时间', max_length=30)
-    end = models.CharField(verbose_name='会议结束时间', max_length=30)
-    duration = models.IntegerField(verbose_name='会议时长', null=True, blank=True)
-    agenda = models.TextField(verbose_name='议程', default='', null=True, blank=True)
-    etherpad = models.CharField(verbose_name='etherpad', max_length=255, null=True, blank=True)
-    emaillist = models.TextField(verbose_name='邮件列表', null=True, blank=True)
-    host_id = models.EmailField(verbose_name='host_id', null=True, blank=True)
-    mid = models.CharField(verbose_name='会议id', max_length=20)
-    password = models.CharField(verbose_name='密码', max_length=128, null=True, blank=True)
-    join_url = models.CharField(verbose_name='进入会议url', max_length=128, null=True, blank=True)
-    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True, null=True, blank=True)
-    is_delete = models.SmallIntegerField(verbose_name='是否删除', choices=((0, '否'), (1, '是')), default=0)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     meeting_type = models.SmallIntegerField(verbose_name='会议类型', choices=((1, 'SIG'), (2, 'MSG'), (3, '专家委员会')),
                                             null=True, blank=True)
-    mmid = models.CharField(verbose_name='腾讯会议id', max_length=20, null=True, blank=True)
     replay_url = models.CharField(verbose_name='回放地址', max_length=255, null=True, blank=True)
     mplatform = models.CharField(verbose_name='第三方会议平台', max_length=20, null=True, blank=True, default='tencent')
 
@@ -141,30 +105,12 @@ class Record(models.Model):
 class Activity(models.Model):
     """活动表"""
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    title = models.CharField(verbose_name='活动标题', max_length=50)
     start_date = models.CharField(verbose_name='活动开始日期', max_length=30)
     end_date = models.CharField(verbose_name='活动结束日期', max_length=30)
     activity_category = models.SmallIntegerField(verbose_name='活动类别',
                                                  choices=((1, '课程'), (2, 'MSG'), (3, '赛事'), (4, '其他')))
-    activity_type = models.SmallIntegerField(verbose_name='活动类型', choices=((1, '线下'), (2, '线上'), (3, '线上与线下')))
-    address = models.CharField(verbose_name='地理位置', max_length=100, null=True, blank=True)
-    detail_address = models.CharField(verbose_name='详细地址', max_length=100, null=True, blank=True)
-    longitude = models.DecimalField(verbose_name='经度', max_digits=8, decimal_places=5, null=True, blank=True)
-    latitude = models.DecimalField(verbose_name='纬度', max_digits=8, decimal_places=5, null=True, blank=True)
     register_method = models.SmallIntegerField(verbose_name='报名方式', choices=((1, '小程序报名'), (2, '跳转链接')))
     online_url = models.CharField(verbose_name='线上链接', max_length=255, null=True, blank=True)
-    register_url = models.CharField(verbose_name='报名链接', max_length=255, null=True, blank=True)
-    synopsis = models.TextField(verbose_name='活动简介', null=True, blank=True)
-    schedules = models.TextField(verbose_name='日程', null=True, blank=True)
-    poster = models.SmallIntegerField(verbose_name='海报', choices=((1, '主题1'), (2, '主题2'), (3, '主题3'), (4, '主题4')),
-                                      default=1)
-    status = models.SmallIntegerField(verbose_name='状态',
-                                      choices=((1, '草稿'), (2, '审核中'), (3, '报名中'), (4, '进行中'), (5, '已结束')), default=1)
-    wx_code = models.TextField(verbose_name='微信二维码', null=True, blank=True)
-    is_delete = models.SmallIntegerField(verbose_name='是否删除', choices=((0, '未删除'), (1, '已删除')), default=0)
-    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
-    sign_url = models.CharField(verbose_name='签到二维码', max_length=255, null=True, blank=True)
-    replay_url = models.CharField(verbose_name='回放地址', max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = "meetings_activity"
--- app_meeting_server/apps/opengauss/models.py
+++ app_meeting_server/apps/opengauss/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from app_meeting_server.utils.models import MyAbstractBaseUser, BaseMeeting
 
 
-class User(models.Model):
+class User(MyAbstractBaseUser):
     """用户表"""
     gid = models.IntegerField(verbose_name='Gitee用户唯一标识')
     gitee_id = models.CharField(verbose_name='GiteeID', max_length=50)
@@ -10,6 +11,8 @@ class User(models.Model):
     email = models.EmailField(verbose_name='邮箱')
     create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True, null=True, blank=True)
     expire_time = models.IntegerField(verbose_name='过期时间', default=0)
+
+    USERNAME_FIELD = 'gitee_id'
 
     class Meta:
         db_table = "meetings_user"
@@ -28,28 +31,11 @@ class Group(models.Model):
         verbose_name_plural = verbose_name
 
 
-class Meeting(models.Model):
+class Meeting(BaseMeeting):
     """会议表"""
-    topic = models.CharField(verbose_name='会议主题', max_length=128)
-    community = models.CharField(verbose_name='社区', max_length=40, null=True, blank=True)
-    group_name = models.CharField(verbose_name='SIG组', max_length=40, null=True, blank=True)
-    sponsor = models.CharField(verbose_name='发起人', max_length=20)
     avatar = models.CharField(verbose_name='发起人头像', max_length=255, null=True, blank=True)
-    date = models.CharField(verbose_name='会议日期', max_length=30)
-    start = models.CharField(verbose_name='会议开始时间', max_length=30)
-    end = models.CharField(verbose_name='会议结束时间', max_length=30)
-    duration = models.IntegerField(verbose_name='会议时长', null=True, blank=True)
-    agenda = models.TextField(verbose_name='议程', default='', null=True, blank=True)
-    etherpad = models.CharField(verbose_name='etherpad', max_length=255, null=True, blank=True)
-    emaillist = models.TextField(verbose_name='邮件列表', null=True, blank=True)
-    host_id = models.EmailField(verbose_name='host_id', null=True, blank=True)
-    mid = models.CharField(verbose_name='会议id', max_length=20)
     timezone = models.CharField(verbose_name='时区', max_length=50, null=True, blank=True)
-    password = models.CharField(verbose_name='密码', max_length=128, null=True, blank=True)
     start_url = models.TextField(verbose_name='开启会议url', null=True, blank=True)
-    join_url = models.CharField(verbose_name='进入会议url', max_length=128, null=True, blank=True)
-    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True, null=True, blank=True)
-    is_delete = models.SmallIntegerField(verbose_name='是否删除', choices=((0, '否'), (1, '是')), default=0)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     mplatform = models.CharField(verbose_name='第三方会议平台', max_length=20, null=True, blank=True, default='zoom')
