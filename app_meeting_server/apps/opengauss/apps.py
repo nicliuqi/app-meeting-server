69ac113adf755e52a6be6d894433191e342f8419
TomNewChao <tom_toworld@163.com>
2023-10-27 16:25:04 +0800

    remove opengauss relation file

--- app_meeting_server/apps/opengauss/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpengaussConfig(AppConfig):
-    name = 'opengauss'
--- app_meeting_server/urls.py
+++ app_meeting_server/urls.py
@@ -24,9 +24,5 @@ elif settings.FOR_MINDSPORE:
     urlpatterns = [
         path('', include('mindspore.urls')),
     ]
-elif settings.FOR_OPENGAUSS:
-    urlpatterns = [
-        path('', include('opengauss.urls')),
-    ]
 else:
     urlpatterns = list()
