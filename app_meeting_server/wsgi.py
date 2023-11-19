cf8c9e72fc1ae72fa26eca7d496378d2c44224ea
TomNewChao <tom_toworld@163.com>
2023-11-19 18:42:05 +0800

    Cancel additional domains

--- app_meeting_server/wsgi.py
+++ app_meeting_server/wsgi.py
@@ -4,7 +4,6 @@ WSGI config for community_meetings project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
