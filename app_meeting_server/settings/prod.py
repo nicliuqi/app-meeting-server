b71032e9889cadab34908a27407f97233bbf097c
TomNewChao <tom_toworld@163.com>
2023-12-02 14:49:23 +0800

    modify the database charset to utf8mb4

--- app_meeting_server/settings/prod.py
+++ app_meeting_server/settings/prod.py
@@ -201,7 +201,8 @@ DATABASES = {
                 'ssl_version': ssl.PROTOCOL_TLSv1_2,
                 'key': MYSQL_TLS_PEM_CONTENT
             },
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': "utf8mb4"
         }
     }
 }
