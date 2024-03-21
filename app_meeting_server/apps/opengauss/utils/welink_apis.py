862de0f6dc7fb240584efd38e02a75f98adc8da1
liuqi <469227928@qq.com>
2024-03-21 11:35:57 +0800

    adjust function to get welink history meetings

--- app_meeting_server/apps/opengauss/utils/welink_apis.py
+++ app_meeting_server/apps/opengauss/utils/welink_apis.py
@@ -44,11 +44,13 @@ def getParticipants(mid):
     meeting = Meeting.objects.get(mid=mid)
     host_id = meeting.host_id
     access_token = createProxyToken(host_id)
+    if not access_token:
+        return 400, {}
     headers = {
         'X-Access-Token': access_token
     }
     uri = '/v1/mmc/management/conferences/history/confAttendeeRecord'
-    meetings_lst = listHisMeetings(host_id)
+    meetings_lst = listHisMeetings(meeting)
     meetings_data = meetings_lst.get('data')
     participants = {
         'total_records': 0,
